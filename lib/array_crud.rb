f948d4261874d4acbad9ab006c67a451e293d17b
Matthew Lewandowski <matthew.lewandowski@wework.com>
2019-09-06 01:24:18 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,35 @@
 def create_an_empty_array
-  
+  [ ]
 end
 
 def create_an_array
-  
+  StarTrek = ["Spock", "Kirk", "Bones", "Chekov"]
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array(StarTrek = ["Spock", "Kirk", "Bones", "Chekov"])
+  StarTrek << "Picard"
 end
 
-def add_element_to_start_of_array(array, element)
-  
+def add_element_to_start_of_array(StarTrek = ["Spock", "Kirk", "Bones", "Chekov"])
+  StarTrek.unshift("Data")
 end
 
-def remove_element_from_end_of_array(array)
-  
+def remove_element_from_end_of_array(StarTrek = ["Spock", "Kirk", "Bones", "Chekov"])
+  StarTrek.pop("Chekov")
 end
 
-def remove_element_from_start_of_array(array)
-  
+def remove_element_from_start_of_array(StarTrek = ["Spock", "Kirk", "Bones", "Chekov"])
+  StarTrek.shift("Spock")
 end
 
-def retrieve_element_from_index(array, index_number)
-  
+def retrieve_element_from_index(StarTrek = ["Spock", "Kirk", "Bones", "Chekov"])
+  puts StarTrek[1]
 end
 
-def retrieve_first_element_from_array(array)
-  
+def retrieve_first_element_from_array(StarTrek = ["Spock", "Kirk", "Bones", "Chekov"])
+  puts StarTrek[0]
 end
 
-def retrieve_last_element_from_array(array)
-  
+def retrieve_last_element_from_array(StarTrek = ["Spock", "Kirk", "Bones", "Chekov"])
+  puts StarTrek[-1]
 end
